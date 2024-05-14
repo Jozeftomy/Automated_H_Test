5f6eaa71c2538058299b9fd42114ce35473704d9
GeorgeET15 <ironmanrocks353@gmail.com>
2024-05-15 05:19:24 +0530

    admin password added

--- client/src/Admin.jsx
+++ client/src/Admin.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
 
 function Admin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   let navigate = useNavigate();
 
   function handleClick() {
-    navigate("/test");
+    if (username === "admin" && password === "admin@123") {
+      navigate("/test");
+    } else {
+      alert("Invalid username or password");
+    }
   }
+
   return (
     <div className="form-container">
       <h2>Login</h2>
-
       <label>
         User id:
-        <input type="email" />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
       <button onClick={handleClick}>Login</button>
     </div>
